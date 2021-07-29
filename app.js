fd8f90151f79bca071c65a408f6e7e0d724959ed
suchitd11 <dsuchit111@gmail.com>
2021-07-29 18:21:15 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { urlencoded } = require("body-parser");
+
+const app = express();
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    var today = new Date();
+    var options = {
+        weekday : "long",
+        day : "numeric",
+        month : "long"
+    };
+
+    var day = today.toLocaleDateString("en-US",options);
+
+    res.render("list", {
+        kindOfDay: day,
+        newListItems : items
+    });
+});
+
+app.post("/", (req,res) => {
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+});
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
