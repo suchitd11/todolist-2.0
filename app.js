ea6869a39c6255d13c4e2a92f59a7f01bd8f2fb8
suchitd11 <dsuchit111@gmail.com>
2021-08-20 00:31:07 +0530

    Version 2.0

--- app.js
+++ app.js
@@ -1,55 +1,132 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const { urlencoded } = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const itemsSchema = {
+    name : String
+};
+
+const Item = mongoose.model("Item",itemsSchema);
+
+const item1 = new Item ({
+    name : "Wake Up!"
+});
+
+const item2 = new Item ({
+    name : "Do Breakfast."
+});
+
+const item3 = new Item ({
+    name : "Study!"
+});
+
+const defaultItems = [item1,item2,item3];
+
+const listSchema = {
+    name : String,
+    items : [itemsSchema]
+};
+
+const List = mongoose.model("List",listSchema);
+
 app.get("/", (req, res) => {
-    let today = new Date();
-    let options = {
-        weekday : "long",
-        day : "numeric",
-        month : "long"
-    };
 
-    let day = today.toLocaleDateString("en-US",options);
+    Item.find({}, (err,foundItems)=>{
+        if (foundItems.length === 0){
+            Item.insertMany(defaultItems,(err)=>{
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    console.log("Successfully saved documents!");
+                }
+            });
+            res.redirect("/");
+        } 
+        else{
+            res.render("list", {
+                listTitle: "Today",
+                newListItems : foundItems
+            });
+        }
+    });
+});
 
-    res.render("list", {
-        listTitle: day,
-        newListItems : items
+app.get("/:customListName", (req,res)=>{
+    const customListName = _.capitalize(req.params.customListName);
+
+    List.findOne({name : customListName},(err,foundListName)=>{
+        if(!err){
+            if(!foundListName){
+                const list = new List ({
+                    name : customListName,
+                    items : defaultItems
+                });
+                list.save();
+                res.redirect("/" + customListName);
+            }
+            else{
+                res.render("list", {
+                    listTitle: foundListName.name,
+                    newListItems : foundListName.items
+                });
+            }
+        }
     });
 });
 
 app.post("/", (req,res) => {
-    let item = req.body.newItem;
+    const itemName  = req.body.newItem;
+    const listName = req.body.list;
 
-    if(req.body.list === "Work"){
-        workItems.push(item);
-        res.redirect("/work");
-    }
-    else{
-        items.push(item);
+    const item = new Item ({
+        name : itemName
+    });
+
+    if(listName === "Today"){
+        item.save();
         res.redirect("/");
+    } else{
+        List.findOne({name : listName}, (err,foundList)=>{
+            foundList.items.push(item);
+            foundList.save();
+            res.redirect("/" + listName);
+        });
     }
 });
 
-app.get("/work", (req,res) => {
-    res.render("list", {
-        listTitle: "Work List", 
-        newListItems: workItems
-    });
+app.post("/delete",(req,res)=>{
+    const checkedItemId = req.body.checkbox;
+    const listName = req.body.listName;
+
+    if (listName === "Today"){
+        Item.findByIdAndRemove(checkedItemId, (err)=>{
+            if(!err){
+                console.log("Successfully deleted checked item!");
+                res.redirect("/");
+            }
+        });
+    } else{
+        List.findOneAndUpdate({name:listName},{$pull: {items: {_id: checkedItemId}}},(err,foundList)=>{
+            if(!err){
+                res.redirect("/" + listName);
+            }
+        });
+    }
 });
 
 app.post("/work", (req,res) => {
-    let item = req.body.newItem;
+    const item = req.body.newItem;
     workItems.push(newItem);
     res.redirect("/work");
 });
